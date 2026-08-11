f8ae913905f5e3edfb455f522e34217918d4e9bb
Beatriz Vaz <beatrizfvaz1207@gmail.com>
2026-08-11 02:00:15 +0100

    Remove secao de ofertas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Navbar from "@/components/layout/Navbar";
 import Hero from "@/components/home/Hero";
 import Features from "@/components/home/Features";
-import Offers from "@/components/home/Offers";
 import About from "@/components/home/About";
 import QuoteRequest from "@/components/home/QuoteRequest";
 import Testimonials from "@/components/home/Testimonials";
@@ -13,7 +12,6 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Features />
-      <Offers />
       <About />
       <QuoteRequest />
       <Testimonials />
